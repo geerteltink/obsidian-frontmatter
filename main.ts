9ea6185deeed443ae91983fc176cf26bd1665ce5
Geert Eltink <geert.eltink@gmail.com>
2024-01-10 11:37:01 +0100

    feat: auto update frontmatter dates

--- main.ts
+++ main.ts
@@ -1,134 +1,146 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { EventRef, Notice, Plugin, TAbstractFile, TFile } from "obsidian";
+import { format } from "date-fns/format";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FrontMatterPlugin extends Plugin {
+	private eventRefs: EventRef[] = [];
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerSyncEvents();
 	}
 
 	onunload() {
-
+		for (const eventRef of this.eventRefs) {
+			this.app.workspace.offref(eventRef);
+		}
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	registerSyncEvents() {
+		const createRef = this.app.vault.on("create", (file: TAbstractFile) =>
+			this.handleFileChange(file)
+		);
+		this.registerEvent(createRef);
+		this.eventRefs.push(createRef);
+
+		const modifyRef = this.app.vault.on("modify", (file: TAbstractFile) =>
+			this.handleFileChange(file)
+		);
+		this.registerEvent(modifyRef);
+		this.eventRefs.push(modifyRef);
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	async loadSettings() {}
+
+	async saveSettings() {}
+
+	async handleFileChange(
+		file: TAbstractFile
+	): Promise<
+		| { status: "ok" }
+		| { status: "error"; error: any }
+		| { status: "ignored" }
+	> {
+		if (!isTFile(file) || ignoreFile(file)) {
+			return { status: "ignored" };
+		}
+
+		const fileContent = await this.app.vault.read(file);
+		let body = fileContent;
+		if (/^(---(.|\n)+?---)/.test(body)) {
+			body = body.replace(/^(---(.|\n)+?---)/g, ``);
+		}
+
+		if (body.length === 0) {
+			return { status: "ignored" };
+		}
+
+		const hash = await sha(body.trim());
+		if (fileContent.contains(hash)) {
+			return { status: "ignored" };
+		}
+
+		try {
+			await this.app.fileManager.processFrontMatter(
+				file,
+				(frontmatter) => {
+					const ctime = formatTimestamp(file.stat.ctime);
+					const mtime = formatTimestamp(file.stat.mtime);
+
+					if (!frontmatter["created"]) {
+						frontmatter["created"] = ctime;
+					}
+
+					if (!frontmatter["modified"]) {
+						frontmatter["modified"] = mtime;
+					}
+
+					if (!frontmatter["hash"]) {
+						frontmatter["hash"] = hash;
+					}
+
+					if (
+						frontmatter["modified"] === mtime ||
+						frontmatter["hash"] === hash
+					) {
+						return;
+					}
+
+					frontmatter["hash"] = hash;
+					frontmatter["modified"] = mtime;
+
+					console.log(
+						`Timestamp updated for "${file.name}"`,
+						frontmatter
+					);
+				}
+			);
+		} catch (e: any) {
+			if (e?.name === "YAMLParseError") {
+				const errorMessage = `Timestamp failed to update because of malformed frontmatter on this file : ${file.path} ${e.message}`;
+				new Notice(errorMessage, 4000);
+				console.error(errorMessage);
+				return {
+					status: "error",
+					error: e,
+				};
+			}
+		}
+
+		return { status: "ok" };
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+function isTFile(value: TAbstractFile): value is TFile {
+	return "stat" in value;
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+function ignoreFile(file: TFile): boolean {
+	if (
+		file.extension !== "md" ||
+		file.parent?.path === "/" ||
+		file.parent?.path.startsWith(".git") ||
+		file.parent?.path.startsWith(".obsidian") ||
+		file.parent?.path.startsWith("archive") ||
+		file.parent?.path.startsWith("assets") ||
+		file.parent?.path.startsWith("templates")
+	) {
+		return true;
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	return false;
+}
 
-		containerEl.empty();
+function formatTimestamp(input: number): String {
+	return format(new Date(input), "yyyy-MM-dd'T'HH:mm");
+}
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+async function sha(content: string) {
+	const msgBuffer = new TextEncoder().encode(content); // encode as UTF-8
+	const hashBuffer = await crypto.subtle.digest("SHA-1", msgBuffer); // hash the message
+	const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert ArrayBuffer to Array
+	const hashHex = hashArray
+		.map((b) => ("00" + b.toString(16)).slice(-2))
+		.join(""); // convert bytes to hex string
+	return hashHex;
 }
